44ef7de54c7a044fa0ea7be068560f1540870c49
UsharaniC <62415928+Usharani-C@users.noreply.github.com>
2020-03-29 21:01:21 +0530

    The Battle of Neighborhoods (Week1)

---
+++ The Battle of Neighborhoods (Week1).py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> 1. Introduction Section <h1>
+# <h2> Discussion on the business problem <h2>
+# <h2> Description of the Problem and Background<h2>
+#     <h3>About :<h3>
+#     <h3> 
+#         The Battle of Neighborhoods to solve the business problem about the Top cuisines and Specific category in the specific area or the area in which the people wish to visit in Dubai.
+#         
+# United Arab Emirates (UAE) state is located on the eastern coast of the Arabic Peninsular, in the southwest corner of the Arabian Gulf. Renowned, worldwide, as a booming centre of international trade and innovation, Dubai has fast become one of the world’s most popular tourist destinations. Only in Dubai can one gaze upon, and travel up, the world’s largest freestanding structure (the Burj Khalifa), shop in both exclusive designer malls and traditional trade souks and indulge in feasts inspired by the flavours of the world while staying in a 7-star hotel!
+# 
+# Dubai is the largest and most populous city in the UAE. There are approximately 2,780,000 residents from over 200 nationalities, plus countless tourists and traders on any given day! The state-of-the-art technology and infrastructure, tax-free living and a strategic location in the centre of the major trading continents, make Dubai the ideal to place to do business. The by-product of this is a dynamic prosperity—the reason that Dubai is one of the leading luxury holiday destinations in the world! Extravagance abounds in this captivating city-state. There is a rich Arabic culture that underpins local life in the city, but all are welcome and encouraged to explore and enjoy Dubai to the absolute fullest! <h3>
+#     
+#     
+#     
+# <h2> Business Problem:
+#     
+#     To find out the best restaurants and different cuisines to enjoy varity of the food in the specific location
+#     
+#     1.Which Indian restaurant is best in Karama and Burduabi Area
+#     2.Which is the Top Korean restaurant
+#     3.Which BBQ restaurant is the best in Dubai
+#     4.Best Donut restaurant 
+#     5.Most visited Coffee Shop 
+#     
+#  <h2> Data <h2>
+#     
+#     
+#  Description of the data and its sources that will be used to solve the problem
+#  
+#  Description of the Data:
+#  
+#  Data is picked from the Kaggle dataset which includes the below details :
+#  
+# •	Address
+# 
+# •	category
+# 
+# •	id
+# 
+# •	lat
+# 
+# •	lng
+# 
+# •	location
+# 
+# •	name
+# 
+# •	originalId
+# 
+# •	polarity
+# 
+# •	subCategory
+# 
+# •	details
+# 
+# •	reviews
+# 
+# 
+# <h2> How the data will be used to solve the problem <h2>
+#     
+# •	Using FourSquare API we will find all venues for each neighborhood.
+# 
+# •	Filter out all venues that are nearby by locality.
+# 
+# •	Using aggregative rating for each resturant to find the best places.
+# 
+# •	Visualize the Ranking of neighborhoods using folium library(python)
+# 
+# 
+
+# 
+#         
+#        
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
